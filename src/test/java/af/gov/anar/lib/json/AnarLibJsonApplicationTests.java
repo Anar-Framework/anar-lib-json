ee7fba9fcb8b76600691739cfe7f64869991a730
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 14:11:45 +0430

    json utility library for anar framework

---
+++ src/test/java/af/gov/anar/lib/json/AnarLibJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
